e09f1ab17cefa838413e063e2b89e55c0e98d63c
Allen Goodman <allen.goodman@icloud.com>
2022-05-25 15:31:45 -0400

    Chebyshev polynomial of the second kind

--- aten/src/ATen/native/Math.h
+++ aten/src/ATen/native/Math.h
@@ -2177,4 +2177,48 @@ static inline C10_HOST_DEVICE T calc_log_ndtr(T x) {
   }
 }
 
+template<typename T>
+static inline T chebyshev_polynomial_u_forward(T x, int64_t n) {
+    if (n < 0) {
+        return T(0.0);
+    }
+
+    if (abs(x) == T(1.0)) {
+        if (x > T(0.0) || n % 2 == 0) {
+            return T(1.0);
+        }
+
+        return T(-1.0);
+    }
+
+    if ((n > 6) && (abs(x) < T(1.0))) {
+        return cos(n * acos(x));
+    }
+
+    if (n == 0) {
+        return T(1.0);
+    }
+
+    if (n == 1) {
+        return x;
+    }
+
+    T p = T(1.0);
+    T q = x;
+    T r;
+
+    for (int k = 2; k <= n; k++) {
+        r = (x + x) * q - p;
+        p = q;
+        q = r;
+    }
+
+    return r;
+}
+
+template<typename T>
+static inline C10_HOST_DEVICE T chebyshev_polynomial_u_forward(T x, T n) {
+    return chebyshev_polynomial_u_forward(x, static_cast<int64_t>(n));
+}
+
 C10_CLANG_DIAGNOSTIC_POP()

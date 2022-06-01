cf8e857e7e9ff9ffc4976a59d656dfe238350be6
Shangdi Yu <yushangdi123@gmail.com>
2022-06-01 23:25:20 +0000

    add documentation

--- cse.py
+++ cse.py
@@ -6,8 +6,13 @@ from functorch.compile import aot_function, print_compile
 import hashlib
 import json
 
-# https://stackoverflow.com/questions/3054449/how-to-properly-define-hash-function-for-a-list-of-objects
-#TODO: another way: recursively change immutable_list to tuple?
+
+#TODO: another way: recursively change immutable_list to tuple? but how about dict nested in list?
+# hash arg and return a number
+# if arg is a list, cast to tuple which is a hashable type
+# for nested unhashable types, recursively hash each element and combine the hashcode of each element
+# for torch.fx.immutable_collections.immutable_dict, sort the disctionary keyss and dump to a json. Then
+# use the hashlib.md5 to get a encoded version of the json. Finally, hash the code
 def fx_hash(arg):
     if isinstance(arg, list): # torch.fx.immutable_collections.immutable_list is also a list
         arg = tuple(arg)
@@ -15,6 +20,7 @@ def fx_hash(arg):
         return hash(arg)
     except TypeError:
         if(isinstance(arg, tuple)):
+            # https://stackoverflow.com/questions/3054449/how-to-properly-define-hash-function-for-a-list-of-objects
             hashCode = 1
             for ele in arg:
                 hashCode = 31*hashCode + (0 if ele is None else fx_hash(ele)) #TODO: but this works for set, but not for ordered list
@@ -27,8 +33,8 @@ def fx_hash(arg):
         else:
             raise TypeError
 
-# def check_same(node, n):
-#     return node.target == n.target and node.args == n.args and node.kwargs == n.kwargs
+
+
 def check_args(new_args, old_args, env):
     if (len(new_args)!=len(old_args)):
         return False
@@ -41,7 +47,15 @@ def check_args(new_args, old_args, env):
             elif new_args[i] != env[old_args[i]]:
                 return False
     return True
-def check_same(new_node, old_node, env):
+
+# check if two nodes new_node and old_node are the same
+# two nodes are the same if
+# 1) they have the same target
+# 2) their args and kwargs are the same. For node the elements of args and kwargs, they are the same
+#    if the old element map to the new element in env. 
+# essentially the following sementic with a more sophisticated implementtation of ==
+#     node.target == n.target and node.args == n.args and node.kwargs == n.kwargs
+def check_same(new_node: torch.fx.node.Node, old_node: torch.fx.node.Node, env: dict):
     if (new_node.target != old_node.target):
         return False
     if not check_args(new_node.args, old_node.args, env):
@@ -50,25 +64,27 @@ def check_same(new_node, old_node, env):
         return False
     return True
 
-def modify(fx_g):
+
+# return a new graph with CSE applied to the input graph
+# env stores a mapping from node in the old graph to node in the new graph
+# The placehold, output, and get_attr nodes are copied to the new grpah without change
+# The call nodes (call_function, call_module, call_method) are hashed to check if they
+# have an equivalent node in the graph. If so, this node will not be copied, and a mapping
+# to the duplicated node is stored in env
+def modify(fx_g: torch.fx.graph_module.GraphModule):
     new_graph = fx.Graph()
     env = {} # map from node in the old graph to node in the new graph
     hash_env = {} # map from the computatio result to a node in the new graph
     for n in fx_g.graph.nodes:
-        if n.op == 'placeholder' or n.op == 'output': # != "call_function"
-            new_node = new_graph.node_copy(n, lambda x: env[x])
-            env[n] = new_node
-        elif n.op =='get_attr':
+        if n.op == 'placeholder' or n.op == 'output' or n.op == 'get_attr': # != "call_function"
             new_node = new_graph.node_copy(n, lambda x: env[x])
             env[n] = new_node
         else: #n.op == 'call_function' or n.op == 'call_module' or n.op == 'call_method'
-            print("======")
-            print(n.target)
-            print(n.args)
-            print(n.kwargs)
+            # print("======")
+            # print(n.target)
+            # print(n.args)
+            # print(n.kwargs)
             # print(n.name) # e.g. cos_1
-
-
             # try:
             args = list(n.args) #convert to list because tuple type is not mutable
             kwargs = list(n.kwargs)
@@ -80,7 +96,7 @@ def modify(fx_g):
                     kwargs[i] = env[kwargs[i]]
             hash_arg = fx_hash([args, kwargs])
             hash_val = (n.target, hash_arg) # (operator, arg) tuple([env[n.args[0]]])
-            if hash_val in hash_env and check_same(hash_env[hash_val], n, env): #   TODO: what if hash collision happened?
+            if hash_val in hash_env and check_same(hash_env[hash_val], n, env): 
                 env[n] = hash_env[hash_val]
                 continue
             # except TypeError:
@@ -94,73 +110,3 @@ def modify(fx_g):
             env[n] = new_node
             
     return new_graph
-
-
-
-
-
-# def f(x):
-#     vals = [x]
-#     for _ in range(5):
-#         vals.append(vals[-1].cos())
-#     return vals[-1]
-
-# a = torch.randn(3, 3, requires_grad=True)
-# fx_g = make_fx(f)(a)
-
-# aot_function(f, print_compile)(a) 
-# # aot_function takes two arguments, a function and something to do after the function.
-# # in this case, it prints one forward pass and one backward pass
-# # not that in backward pass, torch.ops.aten.t(primals_1) is computed twice
-# # it traces the forward and backward graph 
-
-# print(fx_g.graph)
-# print(fx_g.code)
-# exit(0)
-
-# def f(x):
-#     return x.cos().cos()
-
-# fx_g = make_fx(f)(torch.randn(5))
-
-# print(fx_g.graph)
-# print(fx_g.code)
-# for n in fx_g.graph.nodes:
-#     if n.op == 'call_function':
-#         if n.target == torch.ops.aten.cos:
-#             n.target = torch.ops.aten.sin
-
-# fx_g.recompile()
-# print(fx_g.code)
-# t = torch.tensor([0])
-# print(f(t))
-# print(fx_g(t))
-# exit(0)
-
-# def f(x):
-#     return x.cos().cos()
-# fx_g = make_fx(f)(torch.randn(5))
-
-# new_graph = fx.Graph()
-# env = {}
-# hash_env = {}
-# for n in fx_g.graph.nodes:
-#     if n.op != 'call_function':
-#         new_node = new_graph.node_copy(n, lambda x: env[x])
-#         env[n] = new_node
-#     elif n.op == 'call_function':
-#         hash_val = (n.target, tuple([env[n.args[0]]]))
-#         if hash_val in hash_env:
-#             env[n] = hash_env[hash_val]
-#             continue
-
-#         new_node = new_graph.call_function(torch.ops.aten.sin, tuple([env[n.args[0]]]))
-#         hash_env[hash_val] = new_node
-#         env[n] = new_node
-
-# print(new_graph)
-# t = torch.tensor([0])
-# print(f(t))
-# print(type(fx_g))
-# print(fx_g(t))
-# print(fx.GraphModule({"x_1":torch.Tensor},new_graph)(t))
